b852fa6ae83d3efdba9b65111e538577d40ac76e
stan e <yi.hang@live.com>
2018-10-14 23:22:20 +0800

    添加类

---
+++ MyBlog.Infrastructure/Repository/IRepository.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MyBlog.Repository
+{
+    public class IRepository<TEntity,TType>
+        where TEntity : Entity,
+    {
+
+    }
+}
